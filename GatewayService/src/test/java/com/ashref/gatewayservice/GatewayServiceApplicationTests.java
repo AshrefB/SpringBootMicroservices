db256b9c2c42b853fe8d46c51dd5251aa9e214f8
Achref Bouadila <a_bouadila@live.fr>
2020-03-26 00:18:25 +0100

    init GatewayService project

---
+++ GatewayService/src/test/java/com/ashref/gatewayservice/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashref.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
