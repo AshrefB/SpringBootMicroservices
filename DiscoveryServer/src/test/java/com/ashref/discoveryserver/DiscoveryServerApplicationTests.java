3981decfd6db2bae547ac5d176fcf1e0c7100c8c
Achref Bouadila <a_bouadila@live.fr>
2020-03-25 22:58:11 +0100

    init DiscoveryServer project

---
+++ DiscoveryServer/src/test/java/com/ashref/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashref.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
