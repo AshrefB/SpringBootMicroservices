1936418ffaee31a07f2bee6813ae965ab2fc7997
Achref Bouadila <a_bouadila@live.fr>
2020-03-26 02:01:10 +0100

    create UserDTO

---
+++ UserService/src/main/java/com/ashref/userservice/dto/UserDTO.java
@@ -0,0 +1,21 @@
+package com.ashref.userservice.dto;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+	@Size(min = 3, max = 30, message = "Name should have [3-30] chars")
+	private String name;
+	@Email(message = "Email should have a valid email")
+	private String email;
+	@Pattern(regexp = "$[0-9]{6-8}^")
+	private String password;
+}
